e073c5fc2f3e90072272e977d88a9b3e3373240c
payel <payel.nandy2580@gmail.com>
2022-07-26 13:30:37 -0400

    SQL

---
+++ answers/exercise16.sql
@@ -0,0 +1,10 @@
+ SELECT ID,STUDENT_NAME,ADDRESS,CITY,POSTALCODE AS ZIP, COUNTRY FROM STUDENTS;
++----+----------------+-------------------+--------------+-------+----------+
+| ID | STUDENT_NAME   | ADDRESS           | CITY         | ZIP   | COUNTRY  |
++----+----------------+-------------------+--------------+-------+----------+
+|  1 | JANE DOE       | 57 UNION St       | Edinburg     | G13RB | Scotland |
+|  2 | JANE SMITH     | 24 PIKE RD        | PHILADELPHIA | 10800 | USA      |
+|  3 | PAYEL BANERJEE | 15 CAPANO DR      | DELAWARE     | 19701 | USA      |
+|  4 | ALBERT WILSON  | 100 CAPITOL TRAIL | TRENTON      | 12345 | USA      |
++----+----------------+-------------------+--------------+-------+----------+
+4 rows in set (0.00 sec)
