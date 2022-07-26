6e6887ede30d1f81cad28d27353e8ffb20f9c6a8
payel <payel.nandy2580@gmail.com>
2022-07-26 13:35:01 -0400

    SQL

---
+++ answers/exercise17.sql
@@ -0,0 +1,12 @@
+ALTER TABLE STUDENTS RENAME LEARNERS;
+Query OK, 0 rows affected (0.03 sec)
+
+mysql> SHOW TABLES;
++-------------------+
+| Tables_in_mynewdb |
++-------------------+
+| COURSES           |
+| LEARNERS          |
++-------------------+
+2 rows in set (0.00 sec)
+
