218934db136e4ca695593c984a60c5d2125af3af
payel <payel.nandy2580@gmail.com>
2022-07-26 13:26:02 -0400

    SQL

---
+++ answers/exercise15.sql
@@ -0,0 +1,9 @@
+ SELECT * FROM COURSES WHERE COURSES BETWEEN 'C%' AND 'Q%';
++-----------+----------------+--------------+-------------+
+| COURSE_ID | STUDENT_NAME   | COURSES      | CREDITHOURS |
++-----------+----------------+--------------+-------------+
+|         1 | JANE SMITH     | DATA SCIENCE |          10 |
+|         2 | PAYEL BANERJEE | PYTHON       |          25 |
+|         3 | JANE DOE       | JAVA         |          20 |
+|         4 | RAM SINGH      | COLDFUSION   |          15 |
++-----------+----------------+--------------+-------------+
